40cbcbcdc9838730087fe4a1bc3ac21135f40e40
Ishan Malviya <ishan.malviya@gmail.com>
2022-08-23 17:51:53 +0530

    Adds logic to the basic equivalent for maps palyground

--- MapBasicEquivalent.playground/Contents.swift
+++ MapBasicEquivalent.playground/Contents.swift
@@ -1,3 +1,9 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var originalArray = [1, 2, 2, 3, 4, 1, 4, 5]
+var newArray: [Int] = []
+
+for element in originalArray {
+    newArray.append(element * 2)
+}
+print(newArray)
