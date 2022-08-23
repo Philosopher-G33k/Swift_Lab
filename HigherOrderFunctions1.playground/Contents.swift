91ff09947e5ff584dd6debb62aa80122c7731a41
Ishan Malviya <ishan.malviya@gmail.com>
2022-08-23 15:13:51 +0530

    Adds the implementation for map, filter and reduce

--- HigherOrderFunctions1.playground/Contents.swift
+++ HigherOrderFunctions1.playground/Contents.swift
@@ -1,3 +1,55 @@
 import UIKit
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
 
 var greeting = "Hello, playground"
+
+extension Array {
+    func myMap<T> (_ tranform : (Element) -> T) -> [T] {
+        var result: [T] = []
+        result.reserveCapacity(self.count)
+        for x in self {
+            result.append(tranform(x))
+        }
+        return result
+    }
+    
+    func myReduce<T>(_ initialResult:T, _ transform: (T, Element)->T) -> T {
+        var result: T = initialResult
+        
+        for element in self {
+            result = transform(result, element)
+        }
+        
+        return result
+    }
+    
+    func myFilter (_ conditionCheck: (Element) -> Bool) -> [Element]  {
+        var result:[Element] = []
+        result.reserveCapacity(self.count)
+        for x in self {
+            if conditionCheck(x) {
+                result.append(x)
+            }
+        }
+        return result
+    }
+}
+
+print([1,2,3,4,5].myReduce(0, +))
+
+print([1,2,3,4,5].myMap{
+    $0 * 2
+})
+
+print([1,2,3,4,5].myFilter({
+    value in
+    return value <= 3
+}))
+
+
+
+print(["{}", "()", "{)"].myFilter({
+    value in
+    return value == "{}" || value == "()"
+}))
